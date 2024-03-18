c5a1fa25d7b210bf9df13a0d472513981f77e773
motaz99 <motaz.dn.ali@gmail.com>
2024-03-18 15:24:38 +0300

    feat: initiate socket $ connect it with frontend

--- src/app.js
+++ src/app.js
@@ -1,14 +1,44 @@
 const express = require('express');
-require('dotenv').config();
 
 const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const cors = require('cors');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello, World! we will do greate work with this project');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  // Emit a message to all connected clients
+  socket.broadcast.emit('user_connected', { userId: socket.id });
+
+  // socket.on('join_room', (data) => {
+  //   socket.join(data);
+  // });
+
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit('receive_message', data);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
